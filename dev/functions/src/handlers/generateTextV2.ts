0a9978ee95d5de362d166cf763f8704252875b03
Nekoyama141592 <mtaky0904@gmail.com>
2024-11-20 19:44:11 +0900

    change: modelを環境変数にする

--- dev/functions/src/handlers/generateTextV2.ts
+++ dev/functions/src/handlers/generateTextV2.ts
@@ -3,12 +3,12 @@ import { onCall, HttpsError } from 'firebase-functions/v2/https';
 export const generateTextV2 = onCall(async (request) => {
  try {
     const client = new ai.OpenAI({
-        apiKey: process.env.OPENAI_API_KEY
+        apiKey: `${process.env.OPENAI_API_KEY}`
     });
     const { messages } = request.data;
     const chatCompletion = await client.chat.completions.create({
       messages: messages,
-      model: "gpt-4o-mini",
+      model: `${process.env.CHATGPT_MODEL}`,
     });
     const message = chatCompletion.choices[0].message.content;
     return { message }
